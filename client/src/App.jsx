5b065b879782cb99b8642bb86c4698221f96b8ed
stephanezab <stephanezab@gmail.com>
2024-05-31 15:13:21 -0400

    Update App.jsx

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,14 +1,25 @@
 import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
+import { StreamChat } from 'Stream-chat'
+import { Chat } from 'stream-chat-react'
+import Cookies from 'universal-cookie'
 import './App.css'
+
+const apikey = 'yasqhy2bxhjp';
+const client = StreamChat.getInstance(apikey);
 
 function App() {
   
   return (
-    <>
-      
-    </>
+    <div className="app__ wrapper">
+      <Chat client={client}>
+        <ChanellListContainer/>
+
+        <ChannelContainer/>
+         
+      </Chat>
+    </div>
   )
 }
 
