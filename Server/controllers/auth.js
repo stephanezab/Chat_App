bed64d8e7247bb975d3cfa197a4a63f5b8e387b6
stephanezab <stephanezab@gmail.com>
2024-06-10 13:18:46 -0400

    update

--- Server/controllers/auth.js
+++ Server/controllers/auth.js
@@ -1,19 +1,25 @@
 const {connect} = require("getstream")
 const bcrypt = require("bcrypt")
-const StreaChat = require("stream-chat")
+const StreamChat = require("stream-chat")
 const crypto = require("crypto")
 
 const api_key = process.env.STREAM_API_KEY
 const api_secret = process.env.STREAM_API_SECRET
 const app_id = process.env.STREAM_APP_ID
 
-const signup = (req, res) => {
+const signup = async(req, res) => {
     try {
         const { fullName, username, password, phoneNumber} = req.body
         // then a random id for user
         const userId = crypto.randomBytes(16).toString("hex")
     
         const serverClient = connect(api_key, api_secret, app_id)
+
+        const hashedPassword = await bcrypt.hash(password, 10)
+
+        const token = serverClient.createUserToken(userId)
+
+        res.status(200).json({token, fullName, username, userId, hashedPassword, phoneNumber})
 
         
     } catch (error) {
@@ -23,9 +29,30 @@ const signup = (req, res) => {
 }
 
 
-const login = (req, res) => {
+const login = async(req, res) => {
 
     try {
+
+        const {username, password} = req.body
+
+        const serverClient = connect(api_key, api_secret, app_id)
+        const client = StreamChat.getInstance(api_key, api_secret)
+        
+        // we want to query all the user in the database to see which matches this user
+        const {users} = await client.queryUsers({name: username})
+
+        if(!users.length) return res.status(400).json({message: "User not fount"});
+
+        const success = await bcrypt.compare(password, users[0].hashedPassword)
+        const token = serverClient.createUserToken(users[0].id)
+        // why id not userId??????????????
+
+        if(success){
+            res.status(200).json({token, fullName: users[0].fullName, username, userId: users[0].id})
+        }
+        else{
+            res.status(500).json({message: "Incorrect password"})
+        }
         
     } catch (error) {
         console.log(error)
