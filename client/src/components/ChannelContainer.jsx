f63e080338b13172b29db59acd986cf819ee2189
stephanezab <stephanezab@gmail.com>
2024-06-21 10:25:12 -0400

    Update ChannelContainer.jsx

--- client/src/components/ChannelContainer.jsx
+++ client/src/components/ChannelContainer.jsx
@@ -23,7 +23,7 @@ const ChannelContainer = (isCreating, setIsCreating, isEditing, setIsEditing, cr
 
   const EmptyState = () => {
     <div className="channel-empty__container">
-      <p className="channel-empty__first">This is the beginning</p>
+      <p classNae="channel-empty__first">This is the beginning</p>
       <p className="channel-empty__second">Send messages, attachments, links, emojis, and more!</p>
     </div>
   }
