8a2c521752bf2f18dfac23fb3bcefc7655b313e8
stephanezab <stephanezab@gmail.com>
2024-07-03 18:06:48 -0400

    Update ChannelContainer.jsx

--- client/src/components/ChannelContainer.jsx
+++ client/src/components/ChannelContainer.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 import {Channel, useChatContext} from "stream-chat-react"
 import {ChannelInner, CreateChannel, EditChannel, TeamMessage } from "./"
 
-const ChannelContainer = (isCreating, setIsCreating, isEditing, setIsEditing, createType) => {
+const ChannelContainer = ({isCreating, setIsCreating, isEditing, setIsEditing, createType}) => {
   
   const {channel} = useChatContext()
 
